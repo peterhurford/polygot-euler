0d74dcbe5a76721540ea4d7d8b9ef2c82d383552
Peter Hurford <peter@peterhurford.com>
2015-04-26 20:09:35 -0500

    shorten p001 in Ruby

--- p001/p001.rb
+++ p001/p001.rb
@@ -3,19 +3,10 @@
 
 ### Find the sum of all the multiples of 3 or 5 below 1000.
 
-def is_multiple?(x, m)
-  x % m == 0
-end
-
-def is_multiple_of_3_or_5?(n)
-  is_multiple?(n, 3) || is_multiple?(n, 5)
-end
-
 def sum_all_multiples_of_3_or_5_below_n(n)
-  (1..n-1).select { |x| is_multiple_of_3_or_5?(x) }.reduce(:+)
+  (1..n-1).select { |x| x % 3 == 0 || x % 5 == 0 }.reduce(:+)
 end
 
 sum_all_multiples_of_3_or_5_below_n(10)     # 23
 
 sum_all_multiples_of_3_or_5_below_n(1000)   # 233168
-
