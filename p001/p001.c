6e75bc44d484535b0467925b96b7713a76b8959e
Peter Hurford <peter@peterhurford.com>
2015-04-27 16:53:33 -0500

    p001 in C

--- p001/p001.c
+++ p001/p001.c
@@ -1,8 +1,27 @@
+#include <stdio.h>
+
 // If we list all the natural numbers below 10 that are multiples of 3 or 5,
 // we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
-int sum_all_multiples_of_3_or_5_below_n(int n)
+int sum_all_multiples_of_3_or_5_below_n(int n);
 
-// TODO:
+int sum_all_multiples_of_3_or_5_below_n(int n) {
+  int total = 0;
+  n--;
+  while (n >= 0) {
+    if (n % 3 == 0 || n % 5 == 0) { total += n; }
+    n--;
+  }
+  return(total);
+}
+
+int main() {
+  printf("%d", sum_all_multiples_of_3_or_5_below_n(10));   // 23
+  printf("\n");
+  printf("%d", sum_all_multiples_of_3_or_5_below_n(1000)); // 233168
+  printf("\n");
+  return 0;
+}
+
