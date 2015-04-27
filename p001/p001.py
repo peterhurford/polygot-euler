d53b1677dde8750011551b22d11d68e4aeb189e7
Peter Hurford <peter@peterhurford.com>
2015-04-26 20:07:47 -0500

    p001 in Python

---
+++ p001/p001.py
@@ -0,0 +1,11 @@
+### If we list all the natural numbers below 10 that are multiples of 3 or 5,
+### we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+### Find the sum of all the multiples of 3 or 5 below 1000.
+
+def sum_all_multiples_of_3_or_5_below_n(n):
+  return(sum(filter(lambda x: x % 3 == 0 or x % 5 == 0, range(0, n))))
+
+sum_all_multiples_of_3_or_5_below_n(10)    # 23
+
+sum_all_multiples_of_3_or_5_below_n(1000)  # 233168
