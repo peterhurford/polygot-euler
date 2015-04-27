135ce977036d10fb333dd5936a0e2119574aa9c8
Peter Hurford <peter@peterhurford.com>
2015-04-27 14:23:49 -0500

    p001 in C is a TODO (C is the last language I'll attempt for now)

---
+++ p001.c
@@ -0,0 +1,8 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5,
+// we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+int sum_all_multiples_of_3_or_5_below_n(int n)
+
+// TODO:
