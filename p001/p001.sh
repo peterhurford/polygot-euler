711e8cfb8216eb43ee81dca2eb93cd69814f5c1b
Peter Hurford <peter@peterhurford.com>
2015-04-27 15:40:29 -0500

    Bash as todo, 10 languages sounds good enough

---
+++ p001/p001.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+### If we list all the natural numbers below 10 that are multiples of 3 or 5,
+### we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+### Find the sum of all the multiples of 3 or 5 below 1000.
+
+function sum_all_multiples_of_3_or_5_below_n () {
+  local total = 0
+  local n = $1
+}
+
+#TODO:
