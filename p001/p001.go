60030999a97709c1e625edcec8a94b473162a342
Peter Hurford <peter@peterhurford.com>
2015-05-02 17:00:31 -0400

    fix go comments

--- p001/p001.go
+++ p001/p001.go
@@ -4,19 +4,20 @@
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
 package main
+
 import "fmt"
 
-func sumAllMultiplesOf3or5BelowN(n int) int{
-  total := 0
-  for x := n-1; x >= 0; x-- {
-    if (x % 3 == 0 || x % 5 == 0) {
-      total += x
-    }
-  }
-  return total
+func sumAllMultiplesOf3or5BelowN(n int) int {
+	total := 0
+	for x := n - 1; x >= 0; x-- {
+		if x%3 == 0 || x%5 == 0 {
+			total += x
+		}
+	}
+	return total
 }
 
 func main() {
-  fmt.Println(sumAllMultiplesOf3or5BelowN(10))    # 23
-  fmt.Println(sumAllMultiplesOf3or5BelowN(1000))  # 233168
+	fmt.Println(sumAllMultiplesOf3or5BelowN(10))   // 23
+	fmt.Println(sumAllMultiplesOf3or5BelowN(1000)) // 233168
 }
