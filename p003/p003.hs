4e6dec8d96891da052751b5d7d8636f98f77d9b7
Peter Hurford <peter@peterhurford.com>
2015-05-29 22:24:02 -0500

    p003.hs (WIP)

--- p003/p003.hs
+++ p003/p003.hs
@@ -2,7 +2,11 @@
 -- What is the largest prime factor of the number 600851475143?
 
 isPrime :: Int -> Bool
-isPrime n = 
+isPrime n = (null [ x | x <- [2..n-1], (mod n x) == 0])
 
 largestPrimeFactor :: Int -> Int
-largestPrimeFactor n = (maximum [ x | x <- [2..(floor (sqrt n))], (mod n x) == 0, isPrime x])
+largestPrimeFactor n = (maximum [ x | x <- [2..n-1], (mod n x) == 0, isPrime x])
+
+largestPrimeFactor 13195  -- 29
+largestPrimeFactor 600851475143
+-- Super slow because I can't restrict by square root because of Types.
