741bf0a9bb49ca7cb99c5019cdd31e88ee5e7f03
Peter Hurford <peter@peterhurford.com>
2015-06-03 18:53:13 -0500

    Finish p003 in Haskell

--- p003/p003.hs
+++ p003/p003.hs
@@ -2,11 +2,10 @@
 -- What is the largest prime factor of the number 600851475143?
 
 isPrime :: Int -> Bool
-isPrime n = (null [ x | x <- [2..n-1], (mod n x) == 0])
+isPrime n = (null [ x | x <- [2..(floor (sqrt (realToFrac n)))], (mod n x) == 0]) ])
 
 largestPrimeFactor :: Int -> Int
-largestPrimeFactor n = (maximum [ x | x <- [2..n-1], (mod n x) == 0, isPrime x])
+largestPrimeFactor n = (maximum [ x | x <- [2..(floor (sqrt (realToFrac n-1)))], (mod n x) == 0, isPrime x])
 
-largestPrimeFactor 13195  -- 29
-largestPrimeFactor 600851475143
--- Super slow because I can't restrict by square root because of Types.
+largestPrimeFactor 13195         -- 29
+largestPrimeFactor 600851475143  -- 6857
