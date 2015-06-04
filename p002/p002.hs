7ac8a8583ec58ccd08d09e8436ba9b77c3b739a5
Peter Hurford <peter@peterhurford.com>
2015-06-03 19:00:40 -0500

    Do not exceed

--- p002/p002.hs
+++ p002/p002.hs
@@ -5,7 +5,7 @@
 -- By considering the terms in the Fibonacci sequence whose values do not
 -- exceed four million, find the sum of the even-valued terms.
 
-sum [ x | x <- takeWhile (< 4000000) fibs, even x]
+sum [ x | x <- takeWhile (<= 4000000) fibs, even x]
   where
     fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
 -- 4613732
