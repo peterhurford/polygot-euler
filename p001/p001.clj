81e9394749ae62386ab20254e5547d66318eb100
Peter Hurford <peter@peterhurford.com>
2015-06-06 11:36:39 -0400

    use defn

--- p001/p001.clj
+++ p001/p001.clj
@@ -3,13 +3,13 @@
 
 ; Find the sum of all the multiples of 3 or 5 below 1000.
 
-(def multiplesOf3or5Below (fn [n] (reduce + (filter
+(defn multiplesOf3or5Below [n] (reduce + (filter
   (fn [x] (or
     (= (mod x 5) 0)
     (= (mod x 3) 0)
   )) 
   (range n)
-))))
+)))
 
 (multiplesOf3or5Below 10)
 ; 23
