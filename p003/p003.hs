e804f2514094a4cf4a3730f35110f11b16323d83
Peter Hurford <peter@peterhurford.com>
2015-10-22 15:42:35 -0500

    DRY p003.hs with a common roots function

--- p003/p003.hs
+++ p003/p003.hs
@@ -1,11 +1,14 @@
 -- The prime factors of 13195 are 5, 7, 13 and 29.
 -- What is the largest prime factor of the number 600851475143?
 
+roots :: Int -> [Int]
+roots n = [2..(floor $ sqrt $ realToFrac n)]
+
 isPrime :: Int -> Bool
-isPrime n = null [ x | x <- [2..(floor $ sqrt $ realToFrac n)], n `mod` x == 0 ]
+isPrime n = null [ x | x <- roots(n-1), n `mod` x == 0 ]
 
 largestPrimeFactor :: Int -> Int
-largestPrimeFactor n = maximum [ x | x <- [2..(floor $ sqrt $ realToFrac n-1)], n `mod` x == 0, isPrime x]
+largestPrimeFactor n = maximum [ x | x <- roots(n-1), n `mod` x == 0, isPrime x]
 
 main :: IO ()
 main = do
