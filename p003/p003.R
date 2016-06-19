79e77e3d3202f88c83a69b4d7793d936b7a84aad
Peter Hurford <peter@peterhurford.com>
2016-06-19 10:41:29 -0500

    More performant and accurate largest prime factor in R

--- p003/p003.R
+++ p003/p003.R
@@ -1,19 +1,16 @@
 ### The prime factors of 13195 are 5, 7, 13 and 29.
 ### What is the largest prime factor of the number 600851475143?
 
-library(magrittr)
-is_prime <- function(n) {
-  if (n == 1 || n == 2) { return(TRUE) }
-  for (i in seq(2, n-1)) {
-    if (n %% i == 0) { return(FALSE) }
-  }
-  TRUE
-}
-
 largest_prime_factor <- function(n) {
-  n %>% sqrt %>% floor %>% seq(2, .) %>%
-    Filter(function(i) n %% i == 0, .) %>%
-    Filter(is_prime, .) %>% max
+  if (n <= 3) { return(n) }
+  for (j in c(2, 3)) { if (n %% j == 0) { return(prime_factor(n / j)) }}
+  k <- floor(sqrt(n))
+  if (k > 5) {
+    for (i in seq(5, k, by = 6)) {
+      for (j in c(i, i + 2)) { if (n %% j == 0) { return(prime_factor(n / j)) }}
+    }
+  }
+  n
 }
 
 largest_prime_factor(13195)
