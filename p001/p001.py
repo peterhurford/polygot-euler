a7319a912cc386b258cff5ff7b7f55a82f92ca37
Peter Hurford <peter@peterhurford.com>
2017-05-05 12:18:27 -0500

    <3 list comprehensions

--- p001/p001.py
+++ p001/p001.py
@@ -4,7 +4,7 @@
 ### Find the sum of all the multiples of 3 or 5 below 1000.
 
 def sum_all_multiples_of_3_or_5_below_n(n):
-  return(sum(filter(lambda x: x % 3 == 0 or x % 5 == 0, range(n))))
+  return(sum(x for x in range(n) if x % 3 == 0 or x % 5 == 0))
 
 sum_all_multiples_of_3_or_5_below_n(10)    # 23
 
